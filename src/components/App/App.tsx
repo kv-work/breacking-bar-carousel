af51a1e402484891502d2388461dc849f196d74f
kv-work <komaroff4@yandex.ru>
2021-02-17 16:42:55 +0500

    removed unnecessary lines

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,6 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
 import CharacterCarousel from '../CharacterCarousel/CharacterCarousel';
-import { Character } from '../../services/BreakingBadApiService.types';
-import { State } from '../../redux/reducer';
-import BreakingBadApiService from '../../services/BreakingBadApiService';
-import { CharacterAction } from '../../redux/characterReducer';
 
 const App: React.FC = () => {
   return (
